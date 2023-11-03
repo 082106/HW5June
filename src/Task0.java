2e115b71a415c875a284782c4cc021f7bd474e85
Borqna Petrova <borqna.petrova05@gmail.com>
2023-11-03 21:55:44 +0200

    test

--- src/Task0.java
+++ src/Task0.java
@@ -2,7 +2,7 @@ public class Task0 {
     public static void main(String[] args) {
         
         System.out.println("първа задача е решена");
-        
+        System.out.println("първа задача е решена");
     
     }
 }
